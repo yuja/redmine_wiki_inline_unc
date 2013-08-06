37bab7b6f96f970f6da8b64687ac32dbdac7d421
Yuya Nishihara <yuya@tcha.org>
2013-08-07 00:22:49 +0900

    replace unc-like text to hyperlink

--- lib/wiki_inline_unc_patch.rb
+++ lib/wiki_inline_unc_patch.rb
@@ -1,8 +1,21 @@
 require_dependency 'redmine/wiki_formatting/textile/formatter'
 
 module WikiInlineUncHelper
+  def auto_unc_link_to(body, url)
+    escurl  = ERB::Util.html_escape(url)
+    escbody = ERB::Util.html_escape(body)
+    # TODO: icon for unc link?
+    %|<a class="external" href="#{escurl}">#{escbody}</a>|.html_safe
+  end
+  module_function :auto_unc_link_to
+
   def auto_unc_link!(text)
-    text
+    # assumes server name is ascii, but paths may contain non-ascii words
+    text.gsub!(/(^|[\s>:,.])(\\\\[\w\-.]+\\[[:alnum:][:punct:]\\]+)/) do
+      leading, unc = $1, $2
+      url = "file:///#{unc.tr('\\', '/')}"
+      "#{leading}#{auto_unc_link_to(unc, url)}"
+    end
   end
   module_function :auto_unc_link!
 end
