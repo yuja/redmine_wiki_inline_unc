b94482fe33663b4bdebc61be2841df2c0c63cf5f
Yuya Nishihara <yuya@tcha.org>
2013-08-07 00:28:06 +0900

    update description

--- init.rb
+++ init.rb
@@ -1,12 +1,12 @@
 require "#{File.dirname(__FILE__)}/lib/wiki_inline_unc_patch.rb"
 
 Redmine::Plugin.register :redmine_wiki_inline_unc do
-  name 'Redmine Wiki Inline Unc plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Redmine Wiki Inline UNC plugin'
+  author 'Yuya Nishihara'
+  description 'This plugin extends Redmine wiki to auto-link UNC paths'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/yuja/redmine_wiki_inline_unc'
+  author_url 'https://github.com/yuja'
 end
 
 Rails.configuration.to_prepare do
